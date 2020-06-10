a9d8e6f2e3efe2530ea32e38b0c77ed8c34c0dcc
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-06-10 17:30:38 +0530

    Game of SOS

--- app/src/main/java/com/example/sos/MainActivity.java
+++ app/src/main/java/com/example/sos/MainActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button[][]buttons = new Button[3][3];
     private boolean player1 = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
+
+    private TextView Player1;
+    private TextView Player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Player1 = findViewById(R.id.player1);
+        Player1 = findViewById(R.id.player1);
 
 
     }
