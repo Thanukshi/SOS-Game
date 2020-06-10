4ee948d638856d736f062a94eb22302423db797c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-06-10 17:57:51 +0530

    Game of SOS

--- app/src/main/java/com/example/sos/MainActivity.java
+++ app/src/main/java/com/example/sos/MainActivity.java
@@ -61,4 +61,15 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         roundCount++;
 
     }
+
+    private boolean checkWinPlayer(){
+        String buttonFields[][] = new String[3][3];
+
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+            }
+        }
+        return false;
+    }
 }
