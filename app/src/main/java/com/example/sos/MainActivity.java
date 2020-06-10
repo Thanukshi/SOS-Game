aea3423205a78b80b41341ae52f5efc7fd5327ea
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-06-10 17:26:52 +0530

    Game of SOS

--- app/src/main/java/com/example/sos/MainActivity.java
+++ app/src/main/java/com/example/sos/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[][]buttons = new Button[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
